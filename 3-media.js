189c271cc6041c81dd0884c7ad4526612a021239
alika <alikaaalmeid@gmail.com>
2026-03-01 18:49:59 -0300

    Fix media calculation and result output

--- 3-media.js
+++ 3-media.js
@@ -1,3 +1,4 @@
+
 // --- Sistema de média de alunos ---
 // O objetivo deste código é calcular a média de um aluno com base em suas notas e determinar se ele foi aprovado ou reprovado. Identifique os erros presentes no código e corrija-os para que o sistema funcione corretamente.
 
@@ -5,18 +6,25 @@ const nomeAluno = "Mariana";
 const nota1 = 8;
 const nota2 = 5;
 
+//substitui a variavel let pra uma return
+
 function calcularMedia(media) {
   return media = (nota1 + nota2) / 2;
 }
 
+
+//criei uma variavel const que recebe a função
+
 const media = calcularMedia () ;
 
-  let resultado;
+//criei uma variavel chamada status pra receber o resultado da media
+
+  let status;
   if (media >= 7) {
-  resultado = "Aprovado";
+  status = "Aprovado";
 } 
 else {
-  resultado = "Reprovado";
+  status = "Reprovado";
 }
 
-console.log("O aluno " + nomeAluno + " está: " + resultado);
+console.log("O aluno " + nomeAluno + " está: " + status);
