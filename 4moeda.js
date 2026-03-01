1750ac859936c393defc9a340d7a3493aa47906e
alika <alikaaalmeid@gmail.com>
2026-03-01 18:54:37 -0300

    Update and rename 4-moeda.js to 4moeda.js

--- 4moeda.js
+++ 4moeda.js
@@ -1,12 +1,20 @@
 // --- Conversor de Moedas ---
 //  O objetivo deste código é simular um conversor de moedas simples, onde o usuário pode converter um valor em dólares para reais usando uma taxa de câmbio fixa. Identifique os erros presentes no código e corrija-os para que o conversor funcione corretamente.
 
+//tirei as aspas da taxaDolar
+
 const taxaDolar = 5.50;
+
+//substitui valorDolar para valor como esta na const transacao
 
 function converterParaReal(valor) {
   let resultado = valor * taxaDolar;
   return resultado;
 }
+
+
+// tirei as aspas do valor e substitui a virgula por ponto
+
 
 const transacao = {
   usuario: "Carlos",
@@ -14,9 +22,13 @@ const transacao = {
   concluida: true,
 };
 
+//adicionei a funcao pra calcular corretamente e criei uma variavel resultado
+
 const valorFinal = converterParaReal (transacao.valor)
 let resultado;
 if (transacao.concluida == true) {
   console.log("O valor convertido para " + transacao.usuario + " é: " + valorFinal);
 
-} else console.log("Erro no processamento.");
+} else {
+  console.log("Erro no processamento.");
+}
